c8fa4fa89d5bc53c221ac57c868f3b65e9e43da2
doyeon789 <ldyeon789@naver.com>
2026-06-22 12:26:39 +0900

    fix: matplotlib -> matplotlib.pyplot 으로 수정

--- Linear Regression/linear_regression.py
+++ Linear Regression/linear_regression.py
@@ -4,7 +4,7 @@
 """
 
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
